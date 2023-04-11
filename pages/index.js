455b69dd84165005416db5b5e205827ebbbbf113
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-04-11 15:13:58 +0430

    adding next-auth functionality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "@/components/Header";
-const index = () => {
+import { getSession } from "next-auth/react";
+const index = ({ session }) => {
   return (
     <div>
       <Header />
     </div>
   );
 };
-
+export async function getServerSideProps(context) {
+  let session = await getSession(context);
+  console.log(session);
+  return {
+    props: {
+      session,
+    },
+  };
+}
 export default index;
