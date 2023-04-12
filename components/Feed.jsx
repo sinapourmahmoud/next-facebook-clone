30bc04cccf88fb8801d64c65af9c1014d06990e2
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-04-12 19:03:25 +0430

    adding tailwind scrollbar hide

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,7 +4,7 @@ import InputBox from "./InputBox";
 import Post from "./Post";
 const Feed = ({ posts }) => {
   return (
-    <div className="flex-grow mt-4 overflow-y-auto pb-28 h-screen">
+    <div className="flex-grow mt-4 overflow-y-auto scrollbar-hide pb-28 h-screen">
       <div className="mx-auto md:max-w-lg lg:max-w-2xl ">
         <Stories />
         <InputBox />
